9c35faa5ab04253606bfc646eff1ae6528cf8cf1
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2019-10-25 02:01:14 +0400

    init

---
+++ SleepWell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SleepWell
+//
+//  Created by Andrey Chernyshev on 25/10/2019.
+//  Copyright © 2019 Andrey Chernyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
