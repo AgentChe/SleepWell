3ace5cb79689599b4cfca1a24831ad1bd795e695
Alex.Mironov <aleksandr.mironov@simbirsoft.com>
2020-01-29 23:59:09 +0300

    noise statuses

---
+++ SleepWell/Classes/Service/AudioPlayerService/Model/LoadState.swift
@@ -0,0 +1,13 @@
+//
+//  LoadState.swift
+//  SleepWell
+//
+//  Created by Alexander Mironov on 29/01/2020.
+//  Copyright © 2020 Andrey Chernyshev. All rights reserved.
+//
+
+enum LoadState {
+    case cached
+    case loadStarted
+    case loadEnded
+}
