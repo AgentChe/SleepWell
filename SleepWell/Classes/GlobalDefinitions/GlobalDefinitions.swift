88d4931dcee3919f9a34485a5d4fd97ed1f1745d
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2020-07-09 17:37:43 +0300

    build 50, prod

--- SleepWell/Classes/GlobalDefinitions/GlobalDefinitions.swift
+++ SleepWell/Classes/GlobalDefinitions/GlobalDefinitions.swift
@@ -5,14 +5,14 @@
 import UIKit
 
 final class GlobalDefinitions {
-//    static let domainUrl = "https://www.s1eep.app" //prod
-    static let domainUrl = "https://test.s1eep.app" // dev
+    static let domainUrl = "https://www.s1eep.app" //prod
+//    static let domainUrl = "https://test.s1eep.app" // dev
     
-//    static let apiKey = "xPh20WC5EYuboYQ" // prod
-    static let apiKey = "1yyl1WBNrlD2MVK" // dev
+    static let apiKey = "xPh20WC5EYuboYQ" // prod
+//    static let apiKey = "1yyl1WBNrlD2MVK" // dev
     
-//    static let amplitudeAPIKey = "b503251969f4b1d7901d2f7d1388d476" // prod
-    static let amplitudeAPIKey = "dde6c038a32c3082b6debe249fad5d34" // dev
+    static let amplitudeAPIKey = "b503251969f4b1d7901d2f7d1388d476" // prod
+//    static let amplitudeAPIKey = "dde6c038a32c3082b6debe249fad5d34" // dev
     
     static let privacyPolicyUrl = "https://www.s1eep.app/legal/policy"
     static let termsOfServiceUrl = "https://www.s1eep.app/legal/terms"
